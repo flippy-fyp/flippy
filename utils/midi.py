de75707a998b2521a7f954aedc369db48029d66b
lhl2617 <lhlee888@hotmail.com>
2021-02-21 18:44:45 +0000

    Copy midi processing functions from testbench

--- utils/midi.py
+++ utils/midi.py
@@ -1,8 +1,59 @@
+from typing import List
+from utils.sharedtypes import NoteInfo
 import pretty_midi
 from pretty_midi.pretty_midi import PrettyMIDI
 import numpy as np
+import mido
+from itertools import chain
 
 def midi_to_audio(midi_path, fs=44100) -> np.ndarray:
     midi_object: PrettyMIDI = pretty_midi.PrettyMIDI(midi_path)
     audio: np.ndarray = midi_object.fluidsynth(fs=fs)
     return audio
+
+def process_midi_to_note_info(midi_path: str) -> List[NoteInfo]:
+    """
+    https://github.com/flippy-fyp/flippy-quantitative-testbench/blob/main/midi.py
+    """
+    mid = mido.MidiFile(midi_path)
+    ret = process_MidiFile(mid)
+
+    return ret
+
+def process_MidiFile(mid: mido.MidiFile) -> List[NoteInfo]:
+    tempo = get_tempo(mid.tracks[0])
+    track_midi_note_info_ticks: List[List[NoteInfo]] = [
+        process_track(track, mid.ticks_per_beat, tempo)
+        for track in mid.tracks
+    ]
+    # flatten
+    ret: List[NoteInfo] = list(chain.from_iterable(track_midi_note_info_ticks))
+    # sort
+    ret.sort(key=lambda x: x["note_start"])
+    return ret
+
+
+def get_tempo(meta_track: mido.midifiles.tracks.MidiTrack) -> int:
+    for msg in list(meta_track):
+        if hasattr(msg, "tempo"):
+            return msg.tempo
+    raise ValueError("Cannot get track tempo")
+
+
+def process_track(
+    track: mido.midifiles.tracks.MidiTrack, ticks_per_beat: int, tempo: int
+) -> List[NoteInfo]:
+    ret: List[NoteInfo] = []
+    curr_tick = 0
+    for msg in track:
+        curr_tick += msg.time
+        if hasattr(msg, "velocity"):
+            if msg.velocity > 0 and msg.type == "note_on":
+                ret.append(
+                    {
+                        "note_start": mido.tick2second(curr_tick, ticks_per_beat, tempo)
+                        * 1000,
+                        "midi_note_num": msg.note,
+                    }
+                )
+    return ret
---
+++ utils/sharedtypes.py
@@ -0,0 +1,4 @@
+from typing import TypedDict
+class NoteInfo(TypedDict):
+    note_start: float  # note start time (ms)
+    midi_note_num: int  # MIDI note number
