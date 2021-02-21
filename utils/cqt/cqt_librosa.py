25ae3cce1e7a43db59e0ab34c54b09440ae2c9ba
lhl2617 <lhlee888@hotmail.com>
2021-02-21 18:52:54 +0000

    WIP: Add librosa_cqt

---
+++ utils/cqt/cqt_librosa.py
@@ -0,0 +1,12 @@
+import librosa
+import numpy as np
+
+
+# def librosa_cqt(
+#     audio: np.ndarray,
+#     fs: int = 44100,
+#     hop: int = 2048,
+#     fmin: float = 130.8,
+#     fmax: float = 4186.0,
+# ) -> np.ndarray:
+#     pass
