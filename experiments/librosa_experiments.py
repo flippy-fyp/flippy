8e0b7a393573545d2b1169aa87120c41a972cbd0
lhl2617 <lhlee888@hotmail.com>
2021-02-22 00:38:20 +0000

    Add librosa functions

---
+++ experiments/librosa_experiments.py
@@ -0,0 +1,84 @@
+from utils.midi import midi_to_audio
+from utils.cqt.cqt_librosa import (
+    extract_features_librosa_cqt,
+    extract_features_librosa_pseudo_cqt,
+    extract_features_librosa_hybrid_cqt,
+    extract_slice_features_librosa_pseudo_cqt,
+    extract_slice_features_librosa_hybrid_cqt,
+    get_librosa_params,
+)
+import matplotlib.pyplot as plt  # type: ignore
+import librosa  # type: ignore
+import librosa.display  # type: ignore
+import soundfile as sf  # type: ignore
+import numpy as np
+import time
+import pyfftw  # type: ignore
+
+librosa.set_fftlib(pyfftw.interfaces.numpy_fft)
+
+
+def conv_to_wav():
+    audio = midi_to_audio("./tmp/wtk1-prelude1.mid", 44100)
+    sf.write("./tmp/wtk-prelude1.wav", audio, samplerate=44100)
+
+
+def plot_cqt(cqt: np.ndarray, fmin: float, hop_length: int = 2048, fs: int = 44100):
+    fig, ax = plt.subplots()
+
+    img = librosa.display.specshow(
+        cqt.T,
+        sr=fs,
+        x_axis="time",
+        y_axis="cqt_note",
+        ax=ax,
+        fmin=fmin,
+        hop_length=hop_length,
+    )
+    fig.colorbar(img, ax=ax)
+
+    plt.show()
+
+
+def plot_librosa_features(extractor=extract_features_librosa_cqt):
+    audio, _ = librosa.load("./tmp/wtk-prelude1.wav", sr=44100, duration=10, mono=True)
+
+    fmin, n_bins = get_librosa_params()
+    cqt = extractor(audio, fmin, n_bins)
+
+    plot_cqt(cqt, fmin)
+
+
+def plot_librosa_features_stream(extractor=extract_slice_features_librosa_pseudo_cqt):
+    frame_length = 2048
+    audio_stream = librosa.stream(
+        "./tmp/wtk-prelude1.wav",
+        1,
+        frame_length,
+        frame_length,
+        mono=True,
+        fill_value=0,
+        duration=10,
+    )
+
+    fmin, n_bins = get_librosa_params()
+    prev_slice: np.ndarray = np.zeros((1, n_bins))
+    cqt: np.ndarray = np.empty((0, n_bins), dtype=np.float32)
+
+    start_time = time.time()
+    for audio_slice in audio_stream:
+        cqt_slice = extractor(audio_slice, fmin, n_bins, 44100)
+        # save for tmp
+        tmp_slice = cqt_slice
+        # get diff
+        cqt_slice = cqt_slice - prev_slice
+        # clip
+        cqt_slice = cqt_slice.clip(0)  # type: ignore
+        # update in cqt
+        cqt = np.append(cqt, cqt_slice, axis=0)
+        # update prev
+        prev_slice = tmp_slice
+
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time}")
+    plot_cqt(cqt, fmin)
--- utils/cqt/cqt_librosa.py
+++ utils/cqt/cqt_librosa.py
@@ -1,12 +1,186 @@
+from typing import Any, Tuple
 import librosa  # type: ignore
-import numpy as np
+import numpy as np  # type: ignore
 
 
-# def librosa_cqt(
-#     audio: np.ndarray,
+def get_librosa_params(
+    fmin: float = 130.8,
+    fmax: float = 4186.0,
+) -> Tuple[float, int]:
+    """
+    Given fmin and fmax return (quantised fmin, n_bins)
+    """
+    # get n_bins
+    start_midi = round(librosa.hz_to_midi(fmin))
+    end_midi = round(librosa.hz_to_midi(fmax))
+    n_bins = end_midi - start_midi
+
+    # Round fmin to closest note's frequency
+    fmin = librosa.midi_to_hz(start_midi)
+
+    return fmin, n_bins
+
+
+def full_cqt_helper(
+    cqt_func: Any,  # librosa.cqt, librosa.pseudo_cqt or librosa.hybrid_cqt
+    audio: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+    hop: int = 2048,
+) -> np.ndarray:
+
+    # Compute CQT
+    cqt = cqt_func(audio, sr=fs, hop_length=hop, fmin=fmin, n_bins=n_bins)
+    # Transpose so that each row is for a time slice's spectra
+    cqt = cqt.T
+    # Take abs value
+    cqt = np.abs(cqt)
+    # L1 normalize
+    cqt = librosa.util.normalize(cqt, norm=1, axis=1)
+    # Take the first element to pad later
+    cqt_0 = cqt[0]
+    # Calculate diff between consecutive rows
+    cqt = np.diff(cqt, axis=0)
+    # Clip negatives
+    cqt = cqt.clip(0)
+    # Insert the first element
+    cqt = np.insert(cqt, 0, cqt_0, axis=0)
+
+    return cqt
+
+
+def extract_features_librosa_cqt(
+    audio: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+    hop: int = 2048,
+) -> np.ndarray:
+    """
+    Extract features via librosa CQT.
+    """
+    return full_cqt_helper(
+        librosa.cqt,
+        audio,
+        fmin,
+        n_bins,
+        fs=fs,
+        hop=hop,
+    )
+
+
+def extract_features_librosa_pseudo_cqt(
+    audio: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+    hop: int = 2048,
+) -> np.ndarray:
+    """
+    Extract features via librosa pseudo CQT.
+    """
+    return full_cqt_helper(
+        librosa.pseudo_cqt,
+        audio,
+        fmin,
+        n_bins,
+        fs=fs,
+        hop=hop,
+    )
+
+
+def extract_features_librosa_hybrid_cqt(
+    audio: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+    hop: int = 2048,
+) -> np.ndarray:
+    """
+    Extract features via librosa hybrid CQT.
+    """
+    return full_cqt_helper(
+        librosa.hybrid_cqt,
+        audio,
+        fmin,
+        n_bins,
+        fs=fs,
+        hop=hop,
+    )
+
+
+def slice_cqt_helper(
+    cqt_func: Any,  # librosa.cqt, librosa.pseudo_cqt or librosa.hybrid_cqt
+    audio_slice: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+) -> np.ndarray:
+    cqt = cqt_func(
+        audio_slice, sr=fs, hop_length=audio_slice.size, fmin=fmin, n_bins=n_bins
+    )
+    # Transpose so that each row is for a time slice's spectra
+    cqt = cqt.T
+    # Ignore the second element
+    cqt = cqt[:1]
+    # Take abs value
+    cqt = np.abs(cqt)
+    # L1 normalize
+    cqt = librosa.util.normalize(cqt, norm=1, axis=1)
+    return cqt
+
+
+def extract_slice_features_librosa_pseudo_cqt(
+    audio_slice: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+) -> np.ndarray:
+    """
+    Extract features for a audio slice via librosa pseudo CQT.
+    """
+    return slice_cqt_helper(
+        librosa.pseudo_cqt,
+        audio_slice,
+        fmin,
+        n_bins,
+        fs=fs,
+    )
+
+
+def extract_slice_features_librosa_hybrid_cqt(
+    audio_slice: np.ndarray,
+    fmin: float,
+    n_bins: int,
+    fs: int = 44100,
+) -> np.ndarray:
+    """
+    Extract features for a audio slice via librosa hybrid CQT.
+    """
+    return slice_cqt_helper(
+        librosa.hybrid_cqt,
+        audio_slice,
+        fmin,
+        n_bins,
+        fs=fs,
+    )
+
+
+## Experimental: Bad results
+# def extract_slice_features_librosa_cqt(
+#     audio_slice: np.ndarray,
+#     fmin: float,
+#     n_bins: int,
 #     fs: int = 44100,
-#     hop: int = 2048,
-#     fmin: float = 130.8,
-#     fmax: float = 4186.0,
 # ) -> np.ndarray:
-#     pass
+#     """
+#     Extract features for a audio slice via librosa CQT.
+#     """
+#     return slice_cqt_helper(
+#         librosa.cqt,
+#         audio_slice,
+#         fmin,
+#         n_bins,
+#         fs=fs,
+#     )
