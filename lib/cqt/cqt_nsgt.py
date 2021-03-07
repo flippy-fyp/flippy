d2c3a0add128a1b745f3300856da9fc258be2408
Lin Lee <lhl2617@vertex50.doc.ic.ac.uk>
2021-03-07 05:00:45 +0000

    complete audiopreprocessor

--- lib/cqt/cqt_nsgt.py
+++ lib/cqt/cqt_nsgt.py
@@ -1,6 +1,7 @@
+from lib.sharedtypes import ExtractorFunctionType
 from lib.utils import quantise_hz_midi
 from nsgt import CQ_NSGT_sliced, CQ_NSGT  # type: ignore
-from typing import Tuple
+from typing import Callable, Tuple
 import librosa  # type: ignore
 import numpy as np  # type: ignore
 import sys
@@ -20,7 +21,7 @@ def extract_features_nsgt_cqt(
     audio: np.ndarray,
     fmin: float,
     fmax: float,
-    hop_length: int = 2048, # artificial
+    hop_length: int = 2048,  # artificial
     fs: int = 44100,
     multithreading: bool = False,
 ) -> np.ndarray:
@@ -33,18 +34,18 @@ def extract_features_nsgt_cqt(
         reducedform=2,
         multithreading=multithreading,
         matrixform=True,
-        real=True
+        real=True,
     )
     # Forward transform
     cqt = nsgt.forward(audio)
-    # Convert to ndarray 
+    # Convert to ndarray
     cqt = np.asarray(cqt)
     # Transpose so that each row is for a time slice's spectra
     cqt = cqt.T
     # Take abs value
     cqt = np.abs(cqt)
-    
-    # The "hop length" of CQ_NSGT is 100, so to simulate the provided hop_length, approximately split 
+
+    # The "hop length" of CQ_NSGT is 100, so to simulate the provided hop_length, approximately split
     # and average the obtained results--this means that the time is approximate!
     averaged_cqt = np.empty((0, cqt.shape[1]), dtype=np.float32)
     quantized_hop_length = hop_length // 100
@@ -57,7 +58,7 @@ def extract_features_nsgt_cqt(
         averaged_cqt = np.vstack([averaged_cqt, avg])
 
     cqt = averaged_cqt
-    
+
     # L1 normalize
     cqt = librosa.util.normalize(cqt, norm=1, axis=1)
     # Take the first element to pad later
@@ -72,6 +73,21 @@ def extract_features_nsgt_cqt(
     return cqt
 
 
+def nsgt_extractor(
+    fmin: float,
+    fmax: float,
+    hop_length: int = 2048,  # artificial
+    fs: int = 44100,
+    multithreading: bool = False,
+) -> ExtractorFunctionType:
+    def w(audio: np.ndarray) -> np.ndarray:
+        return extract_features_nsgt_cqt(
+            audio, fmin, fmax, hop_length, fs, multithreading
+        )
+
+    return w
+
+
 def get_slicq_engine(
     sl_len: int,
     sl_tr_ratio: int,
@@ -81,7 +97,7 @@ def get_slicq_engine(
     multithreading: bool = False,
 ) -> CQ_NSGT_sliced:
     """
-    Get slicq engine. 
+    Get slicq engine.
     """
     tr_len = sl_len // sl_tr_ratio
     return CQ_NSGT_sliced(
@@ -94,7 +110,7 @@ def get_slicq_engine(
         matrixform=True,
         reducedform=2,
         multithreading=multithreading,
-        real=True
+        real=True,
     )
 
 
@@ -111,7 +127,7 @@ def extract_features_nsgt_slicq(
     signal = (audio_slice,)
     # Forward transform
     cqt = slicq.forward(signal)
-    # Convert to ndarray 
+    # Convert to ndarray
     cqt = np.asarray(list(cqt))
     # Take abs value
     cqt = np.abs(cqt)
@@ -119,8 +135,8 @@ def extract_features_nsgt_slicq(
     cqt = np.average(cqt, axis=0)
     # Transpose so that each row is for a time slice's spectra
     cqt = cqt.T
-    # Take only the slice region of the time slices 
-    cqt = cqt[:len(cqt) // sl_tr_ratio]
+    # Take only the slice region of the time slices
+    cqt = cqt[: len(cqt) // sl_tr_ratio]
     # Average over the time slices
     cqt = np.average(cqt, axis=0)
     # Reshape
@@ -129,3 +145,19 @@ def extract_features_nsgt_slicq(
     cqt = librosa.util.normalize(cqt, norm=1, axis=1)
 
     return cqt
+
+
+def nsgt_slicq_extractor(
+    sl_len: int,
+    sl_tr_ratio: int,
+    fmin: float = 130.8,
+    fmax: float = 4186.0,
+    fs: int = 44100,
+    multithreading: bool = False,
+) -> ExtractorFunctionType:
+    slicq = get_slicq_engine(sl_len, sl_tr_ratio, fmin, fmax, fs, multithreading)
+
+    def w(audio_slice: np.ndarray) -> np.ndarray:
+        return extract_features_nsgt_slicq(slicq, sl_tr_ratio, audio_slice)
+
+    return w
