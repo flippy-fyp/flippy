bd03aa3b5a84949fa84cd517e0fb209edfdfb7e3
Lin Lee <lhl2617@vertex50.doc.ic.ac.uk>
2021-03-08 03:08:12 +0000

    Change follower type signature

--- lib/components/follower.py
+++ lib/components/follower.py
@@ -9,15 +9,15 @@ class Follower:
         dtw: str,
         max_run_count: int,
         search_window: int,
-        p_queue: "mp.Queue[np.ndarray]",
-        s_queue: "mp.Queue[np.ndarray]",
+        P_queue: "mp.Queue[np.ndarray]",
+        S: np.ndarray,
     ):
         self.mode = mode
         self.dtw = dtw
         self.max_run_count = max_run_count
         self.search_window = search_window
-        self.p_queue = p_queue
-        self.s_queue = s_queue
+        self.P_queue = P_queue
+        self.S = S
 
         if mode == "online":
             raise NotImplementedError("Online mode not yet implemented!")
