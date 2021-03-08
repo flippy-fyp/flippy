db632a9a6c466d4792f6c1f321f65232c804a46d
Lin Lee <lhl2617@vertex50.doc.ic.ac.uk>
2021-03-08 00:50:13 +0000

    Add mputils tests

---
+++ tests/lib/test_mputils.py
@@ -0,0 +1,30 @@
+from typing import List
+from lib.mputils import consume_queue, produce_queue, write_list_to_queue
+import unittest
+import multiprocessing as mp
+from hypothesis import given, settings, strategies as st
+
+
+class TestMPUtils(unittest.TestCase):
+    @given(st.lists(st.integers(), min_size=20))
+    @settings(max_examples=20)
+    def test_produce_consume_queues(self, l: List[int]):
+        q = produce_queue(l)
+        ll = consume_queue(q)
+        self.assertEqual(l, ll)
+
+    @given(st.lists(st.integers(), min_size=20))
+    @settings(max_examples=20)
+    def test_multiproc_queues(self, l: List[int]):
+        q: "mp.Queue[int]" = mp.Queue()
+        producer_proc = mp.Process(
+            target=write_list_to_queue,
+            args=(
+                l,
+                q,
+            ),
+        )
+        producer_proc.start()
+        ll = consume_queue(q)
+        producer_proc.join()
+        self.assertEqual(l, ll)
