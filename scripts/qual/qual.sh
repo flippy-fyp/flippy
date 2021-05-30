019a57c9a429a7d88dab08cfc96b3b1435a4d539
Lin Lee <lhlee888@hotmail.com>
2021-05-30 18:09:10 +0100

    Add qualitative testbench scripts

---
+++ scripts/qual/qual.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Ensure we're in the project root
+cd "$(dirname "$0")" && cd ../..
+
+if [ "$#" -ne 4 ]; then
+    echo "Need four parameters: piece group, piece name, udphost, udpport"
+    exit 1
+fi
+
+piece_group=$1
+piece_name=$2
+udphost=$3
+udpport=$4
+
+echo "Piece Group: $piece_group"
+echo "Piece Name: $piece_name"
+echo "UDP Host: $udphost"
+echo "UDP Port: $udpport"
+
+perf_wave_path="data/qual/$piece_group/$piece_name/$piece_name.wav"
+score_midi_path="data/qual/$piece_group/$piece_name/$piece_name.mid"
+
+echo "perf_wave_path: $perf_wave_path"
+echo "score_midi_path: $score_midi_path"
+
+./scripts/qual/run.sh "$perf_wave_path" "$score_midi_path" "$udphost" "$udpport"
