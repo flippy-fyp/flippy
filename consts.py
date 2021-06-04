dd1072190e24fb920a7421fddd3e70c54fce37c6
Lin Lee <lhlee888@hotmail.com>
2021-06-04 17:47:25 +0100

    Remove bach10 patch

--- consts.py
+++ consts.py
@@ -7,5 +7,5 @@ BWV846_PATH = os.path.join(DATA_PATH, "bwv846")
 REPRO_RESULTS_PATH = os.path.join(REPO_ROOT, "repro_results")
 EXPERIMENTAL_RESULTS_PATH = os.path.join(REPO_ROOT, "experimental_results")
 BACH10_PATH = os.path.join(DATA_PATH, "bach10", "Bach10_v1.1")
-BACH10_PATCH_PATH = os.path.join(DATA_PATH, "bach10", "Bach10_patch")
+
 MISALIGN_THRESHOLD_MS_RANGE = range(50, 3050, 50)
