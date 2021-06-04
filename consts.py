4e3bbf2bea201aa68cc776ce207494b27e17d17c
Lin Lee <lhlee888@hotmail.com>
2021-06-04 18:01:43 +0100

    Add Bach10 submodule

--- consts.py
+++ consts.py
@@ -6,6 +6,6 @@ DATA_PATH = os.path.join(REPO_ROOT, "data")
 BWV846_PATH = os.path.join(DATA_PATH, "bwv846")
 REPRO_RESULTS_PATH = os.path.join(REPO_ROOT, "repro_results")
 EXPERIMENTAL_RESULTS_PATH = os.path.join(REPO_ROOT, "experimental_results")
-BACH10_PATH = os.path.join(DATA_PATH, "bach10", "Bach10_v1.1")
+BACH10_PATH = os.path.join(DATA_PATH, "Bach10_v1.1")
 
 MISALIGN_THRESHOLD_MS_RANGE = range(50, 3050, 50)
